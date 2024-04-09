2f170d58e5e5213d89de71d1048da341987343ed
Austin <austin.akerley@outlook.com>
2024-04-09 17:43:24 -0500

    Removing some compiled files

---
+++ crypto/cryptanalysis/list_small_primes.py
@@ -0,0 +1,23 @@
+# Title: Small Primes Generator
+# Creator: Austin Akerley
+# Date Created: 11/26/2019
+# Last Editor: Austin Akerley
+# Date Last Edited: 01/18/2019
+# Associated Book Page Nuber: N/A
+
+# INPUT(s) -
+# max - type: int, desc: The value to find all prime integers under
+
+def list_small_primes(max): #max being
+    small_primes = [2,3,5]
+    for i in range (7,max):
+        is_prime = True
+        for prime in small_primes:
+            if i%prime == 0:
+                is_prime = False
+        if is_prime:
+            small_primes.append(i)
+    return small_primes
+
+# OUTPUT - type: list
+# small_primes - type: list of integers, desc: complete list of primes integers les than max
